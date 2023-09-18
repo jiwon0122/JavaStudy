818d8db174e0a4ee1c65cebabe2da257adcd7f91
jini010825 <145331016+jini010825@users.noreply.github.com>
2023-09-18 17:36:04 +0900

    자바스터디 1주차

---
+++ 예진/one week/Rectangle.java
@@ -0,0 +1,43 @@
+package study6;
+
+public class Rectangle extends Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(String color, double width, double height) {
+        super(color);
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @Override
+    public double calculateArea() {
+        return width * height;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * (width + height);
+    }
+
+    @Override
+    public String toString() {
+        return "직사각형: " + super.toString() + ", 가로 = " + width + ", 세로 = " + height;
+    }
+}
