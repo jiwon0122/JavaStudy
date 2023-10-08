dc1ac39c2d88137014ea7718832a5986db94da88
hyunseong <rlagustjd2684@gmail.com>
2023-10-09 08:42:21 +0900

    3주차

---
+++ 현성/threeWeek/hsSimple.java
@@ -0,0 +1,9 @@
+package threeWeek;
+//예금 단리
+public class hsSimple extends hsDeposit{
+
+	public hsSimple(String name, String account_num, int date, double rate, int principal) {
+		super(name, account_num, date, rate, principal);
+	}
+
+}
